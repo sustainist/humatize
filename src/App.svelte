62f8c9b0ef20ac349e9d2e4df370c02411072b92
alinstan1 <alinwillwide@gmail.com>
2026-07-13 01:50:24 +0300

    up

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,5 @@
-<script>
-    import App from "./App.svx";
-    // import FastFest from "./FastFest.svelte";
-    // import HumatizeManifest from "./HumatizeManifest.svelte";
-</script>
-
-<App />
-<!-- <FastFest />
-<HumatizeManifest /> -->
+<center class="humatize-manifest">
+    <h1>HUMATIZE</h1>
+    <h2>Sustainable Crowdfunding</h2>
+    🚧
+</center>
